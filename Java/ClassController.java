c492472a589ed5552317d08389730845fa0f9d3c
luqmanarifin <l.arifin.siswanto@gmail.com>
2015-03-25 11:37:09 +0700

    Java included
    + bonus beberapa referensi

---
+++ Java/ClassController.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.BigInteger;
+
+public class ClassController {
+  public ClassController() {
+  
+  }
+  public ClassController(ClassController c) {
+  
+  }
+  public void ExecuteExpression(String str) {
+  
+  }
+  public void ExecuteCommand(String str) {
+  
+  }
+  public void Undo(int n) {
+  
+  }
+  public void Redo(int n) {
+  
+  }
+  public void ShowMem(int n) {
+  
+  }
+  public void ShowMemAll() {
+  
+  }
+}
---
+++ Java/Reader.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.BigInteger;
+
+public class Reader {
+  public Reader() {
+  
+  }
+  public Reader(Reader r) {
+  
+  }
+  public String Read() {
+  
+  }
+  public String GetSentence() {
+  
+  }
+  public boolean IsEquation() {
+  
+  }
+  private void _UpdateIsEquation() {
+  
+  }
+  private String _senten;
+  private boolean _isEquation;
+  private static final String _commands[];
+}
